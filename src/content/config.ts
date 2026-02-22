22319bffa1260bd410cbacf2873c5acefc4ef0fb
Justin <chenyouduan@gmail.com>
2026-02-22 21:40:07 +0800

    Add: bp-together context

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,15 @@ import { glob } from "astro/loaders";
 
 const projectsCollection = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/projects" }),
-  schema: z.object({
-    title: z.string(),
-    subtitle: z.string().optional(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    image: z.string().optional(),
-    tags: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      subtitle: z.string().optional(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      image: image(),
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = {
